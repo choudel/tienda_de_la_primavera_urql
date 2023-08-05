9ff8d7f9059c273375184335de5f580400aef8b0
Choudel <61978194+choudel@users.noreply.github.com>
2023-08-05 20:49:23 +0100

    fixed auth storage jwt

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script async script lang="ts">
 	import { cacheExchange, fetchExchange, gql, setContextClient, Client } from '@urql/svelte';
+
 	import { authExchange } from '@urql/exchange-auth';
+	import { goto } from '$app/navigation';
 	const tempClient = new Client({
 		url: 'http://localhost:3000/graphql',
 		exchanges: [cacheExchange, fetchExchange]
 	});
 	setContextClient(tempClient);
+	let token1 = '';
+
 	async function initializeAuthState() {
 		const authenticateMutation = gql`
 			mutation MyMutation($email: String = "", $password: String = "") {
@@ -19,21 +23,35 @@
 			password: 'secret1'
 		});
 		const jwt = result.data?.authenticate.jwt;
-
+		localStorage.setItem('token', jwt);
 		return jwt;
+	}
+	function logout() {
+		goto('/a');
 	}
 	const client = new Client({
 		url: 'http://localhost:3000/graphql',
 		exchanges: [
 			cacheExchange,
 			authExchange(async (utils) => {
-				let token = await initializeAuthState();
+				let token = localStorage.getItem('token') || (await initializeAuthState());
 				console.log(token);
 				return {
 					addAuthToOperation(operation) {
 						return utils.appendHeaders(operation, {
 							Authorization: `Bearer ${token}`
 						});
+					},
+					didAuthError(error, _operation) {
+						if (error.response.status === 403) {
+							this.refreshAuth();
+							return true;
+						}
+						return false;
+					},
+					async refreshAuth() {
+						localStorage.clear();
+						logout();
 					}
 				};
 			}),
